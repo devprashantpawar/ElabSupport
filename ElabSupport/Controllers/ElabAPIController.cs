649e8762aac5981885c4a244698fb20796d00969
prashantpawar1725 <prashantpawar1725@gmail.com>
2023-12-15 19:00:58 +0530

    Login API

---
+++ ElabSupport/Controllers/ElabAPIController.cs
@@ -0,0 +1,46 @@
+﻿using ElabSupport.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ElabSupport.Controllers
+{
+    [System.Web.Http.Route("api/[controller]")]
+    public class ElabAPIController : Controller
+    {
+        // GET: API
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [System.Web.Http.HttpPost]
+        public async Task<ActionResult> GetLogin([FromBody] LoginModel model)
+        {
+            UserDAC uc = new UserDAC();
+            DataTable data = null;
+            data = uc.GetLogin(model.UserName, model.Password);
+            if (data != null && data.Rows.Count != 0)
+            {
+                string userid = data.Rows[0].Field<string>("UserId").ToString();
+                ExotelController ec = new ExotelController();
+                var Data = await ec.GetUserData(userid);
+                string jsonData = JsonConvert.SerializeObject(Data);
+                jsonData = jsonData.Replace("\\", "");
+                return Json(jsonData, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+    }
+
+}
